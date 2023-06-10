64d883aae691d13630849f49ca5279123706a697
Ingo Mörl <i.moerl@bfw-schoemberg.de>
2023-06-10 23:26:21 +0200

    template

--- planApp1/views.py
+++ planApp1/views.py
@@ -1,7 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Baustelle")
+    sequenz = Sequenz.objects.filter()
+    context = {
+        'sequenz': sequenz,
+    }
+    return render(request, 'planApp1/plan.html', context)
